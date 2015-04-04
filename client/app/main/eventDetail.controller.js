d3ba54c1248b7a6b3f4a29439fa7002f11bcce40
diennguyen90 <dienM.Nguyen90@gmail.com>
2015-04-04 01:43:37 -0700

    testing with comments

--- client/app/main/eventDetail.controller.js
+++ client/app/main/eventDetail.controller.js
@@ -4,7 +4,7 @@
 angular.module('diennguyenApp')
 	.controller('EventDetailCtrl', function($scope,$routeParams,$http){
 		$scope.events = []
-
+		//gets the data and on success it does a filter on that array to find a matching $routeparam name
 		$http.get('/api/events').success(function(events){
 			$scope.events = events.filter(function(filterEvents){
 				return filterEvents.name === $routeParams.name
